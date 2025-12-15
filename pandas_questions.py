a7b31e9f851a792fcda075202ab94579b6ef2429
baptistepras <prasbaptiste74@gmail.com>
2025-12-15 11:37:14 +0100

    UP my solution

--- pandas_questions.py
+++ pandas_questions.py
@@ -117,7 +117,12 @@ def plot_referendum_map(referendum_result_by_regions):
         gdf_regions['Choice A'] /
         (gdf_regions['Choice A'] + gdf_regions['Choice B'])
     )
-    gdf_regions.plot(column='ratio', legend=True, cmap='Blues', edgecolor='black')
+    gdf_regions.plot(
+        column='ratio', 
+        legend=True, 
+        cmap='Blues', 
+        edgecolor='black'
+    )
 
     return gdf_regions
 
